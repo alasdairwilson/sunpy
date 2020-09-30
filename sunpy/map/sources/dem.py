cb13aa2decfc2576ba0613c5ccd01d134b4e8df5
Alasdair Wilson <alasdair.wlsn@gmail.com>
2020-09-30 19:55:28 +0100

    initial attempt at map source for DEM

--- sunpy/map/sources/dem.py
+++ sunpy/map/sources/dem.py
@@ -15,15 +15,10 @@ __all__ = ['DEMMap']
 class DEMMap(GenericMap):
     """DEM Image Map.
 
-    The Atmospheric Imaging Assembly is a set of four telescopes that employ
-    normal-incidence, multi-layer coated optics to provide narrow-band imaging
-    of the Sun. It provides high resolution full-disk images of the corona and
-    transition region up to 0.5 solar radii above the solar limb with 1.5
-    arcsecond angular resolution and 12-second temporal resolution. It observes
-    the Sun in the following seven extreme ultraviolet bandpasses: 94 A
-    (Fe XVIII), 131 A (Fe VIII, XXI), 171 A (Fe IX), 193 A (Fe XII, XXIV),
-    211 A (Fe XIV), 304 A (He II), 335 A (Fe XVI). One telescope observes
-    in the visible 1600 A (C IV) and the nearby continuum (1700 A).
+    DEMs are recovered from multiwavelength solar instrumental data which, in combination with these
+    instruments temperature responses. The following reads in DEM maps consisting of pixel specific temperature
+    information generally averaged over some t range. These are are nominally produced by demregpy.
+
 
     Notes
     -----
@@ -31,27 +26,17 @@ class DEMMap(GenericMap):
     coordinate systems, including Heliocentric Aries Ecliptic (HAE) and Heliographic Stonyhurst
     (HGS).  SunPy uses the provided HAE coordinates due to accuracy concerns with the provided
     HGS coordinates, but other software packages may make different choices.
-
-    References
-    ----------
-    * `SDO Mission Page <https://sdo.gsfc.nasa.gov/>`_
-    * `Instrument Page <https://aia.lmsal.com>`_
-    * `Fits Header keywords <http://jsoc.stanford.edu/doc/keywords/AIA/AIA02840_A_AIA-SDO_FITS_Keyword_Documents.pdf>`_
-    * `Analysis Guide <https://www.lmsal.com/sdodocs/doc/dcur/SDOD0060.zip/zip/entry/>`_
-    * `Instrument Paper <https://doi.org/10.1007/s11207-011-9776-8>`_
-    * `wavelengths and temperature response reference <https://www.lmsal.com/sdodocs/doc/dcur/SDOD0060.zip/zip/entry/figures/aia_tel_resp.png>`_
     """
 
     def __init__(self, data, header, **kwargs):
         super().__init__(data, header, **kwargs)
 
         # Fill in some missing info
-        self.meta['detector'] = self.meta.get('detector', "AIA")
+        self.meta['detector'] = self.meta.get('detector', "DEM")
         self._nickname = self.detector
         self.plot_settings['cmap'] = None # self._get_cmap_name()
         self.plot_settings['norm'] = ImageNormalize(
             stretch=source_stretch(self.meta, AsinhStretch(0.01)), clip=False)
-
     @property
     def _supported_observer_coordinates(self):
         return [(('haex_obs', 'haey_obs', 'haez_obs'), {'x': self.meta.get('haex_obs'),
@@ -71,7 +56,7 @@ class DEMMap(GenericMap):
 
     @classmethod
     def is_datasource_for(cls, data, header, **kwargs):
-        """Determines if header corresponds to an AIA image"""
-        return str(header.get('bunit', '')).startswith('LOG 10 cm^-5 K^-1')
+        """Determines if units are consistent with a DEM"""
+        return str(header.get('detector', '')).startswith('dem')
 
 
