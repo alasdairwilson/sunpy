f8ce01e940e9adc9b8adcee64825c3124ff62f26
Alasdair Wilson <alasdair.wlsn@gmail.com>
2020-10-28 15:25:53 +0000

    cahnged plot settings

--- sunpy/map/sources/dem.py
+++ sunpy/map/sources/dem.py
@@ -17,7 +17,7 @@ class DEMMap(GenericMap):
 
     DEMs are recovered from multiwavelength solar instrumental data which, in combination with these
     instruments temperature responses. The following reads in DEM maps consisting of pixel specific temperature
-    information generally averaged over some t range. These are are nominally produced by demregpy.
+    information generally averaged over some t range. These are are currently produced by demregpy.
 
 
     Notes
@@ -34,9 +34,12 @@ class DEMMap(GenericMap):
         # Fill in some missing info
         self.meta['detector'] = self.meta.get('detector', "DEM")
         self._nickname = self.detector
-        self.plot_settings['cmap'] = None # self._get_cmap_name()
-        self.plot_settings['norm'] = ImageNormalize(
-            stretch=source_stretch(self.meta, AsinhStretch(0.01)), clip=False)
+        self.plot_settings['cmap'] = 'inferno' # self._get_cmap_name()
+        self.plot_settings['norm'] = ImageNormalize(vmin=0,vmax=255)
+    @property
+    def measurement(self):
+        
+        return self.meta['instrume']      
     @property
     def _supported_observer_coordinates(self):
         return [(('haex_obs', 'haey_obs', 'haez_obs'), {'x': self.meta.get('haex_obs'),
@@ -56,7 +59,9 @@ class DEMMap(GenericMap):
 
     @classmethod
     def is_datasource_for(cls, data, header, **kwargs):
-        """Determines if units are consistent with a DEM"""
+        """
+        returns if map is consistent with a DEM
+        """
         return str(header.get('detector', '')).startswith('dem')
 
 
